f175382fa5cd9bd78a069d4de3271fcfd8c34b91
Igor Bortolotti <arcanjo.bortolotti@gmail.com>
2024-09-04 21:30:39 -0300

    Aula-17 evento de erro

--- main.js
+++ main.js
@@ -3,16 +3,22 @@ const form = document.getElementById("investment-form");
 
 function renderProgression(evt) {
   evt.preventDefault();
-//const startingAmount = Number(form['stating-amout'].value); - Forma alternativa e acessar os valores dos campos.
-  const startingAmount = Number(document.getElementById("starting-amount").value);
-  const additionalContribution = Number(document.getElementById(
-    "additional-contribution"
-  ).value);
+  //const startingAmount = Number(form['stating-amout'].value); - Forma alternativa e acessar os valores dos campos.
+  const startingAmount = Number(
+    document.getElementById("starting-amount").value.replace(",", ".")
+  );
+  const additionalContribution = Number(
+    document.getElementById("additional-contribution").value.replace(",", ".")
+  );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -25,4 +31,27 @@ function renderProgression(evt) {
 
   console.log(returnsArray);
 }
-form.addEventListener('submit', renderProgression);
+
+function validateInput(evt) {
+  if (evt.target.value === "") {
+    return;
+  }
+
+  const { parentElement } = evt.target;
+  const grandParentElement = evt.target.parentElement.parentElement;
+  const inputValue = evt.target.value.replace(",", ".");
+
+  if (isNaN(inputValue) || Number(inputValue) <= 0) {
+    const erroTextElement = document.createElement("p");
+    erroTextElement.classList.add("text-red-500");
+    erroTextElement.innerText = "Insira um valor númerico e maior que zero";
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(erroTextElement);
+  }
+}
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
+}
+form.addEventListener("submit", renderProgression);
