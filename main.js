06f8f629be0445481193d366bb6a09ea020990eb
iBortolotti <arcanjo.bortolotti@gmail.com>
2024-10-23 16:57:57 -0300

    Aula 29 - Resetando os Graficos

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 import { generateReturnsArray } from "./src/investmentGoals";
 import { Chart } from "chart.js/auto";
 
-
 const form = document.getElementById("investment-form");
 const clearFormButton = document.getElementById("clear-form");
 const finalMoneyChart = document.getElementById("final-money-distribution");
 const progressionChart = document.getElementById("progression");
 
+let doughnutChartReference = {};
+let progressionChartReference = {};
+
 function formatCurrency(value) {
-  return value.toFixed(2)
+  return value.toFixed(2);
 }
 
 function renderProgression(evt) {
@@ -16,22 +18,16 @@ function renderProgression(evt) {
   if (document.querySelector(".error")) {
     return;
   }
+  
+  resetCharts();
   //const startingAmount = Number(form['stating-amout'].value); - Forma alternativa e acessar os valores dos campos.
-  const startingAmount = Number(
-    document.getElementById("starting-amount").value.replace(",", ".")
-  );
-  const additionalContribution = Number(
-    document.getElementById("additional-contribution").value.replace(",", ".")
-  );
+  const startingAmount = Number(document.getElementById("starting-amount").value.replace(",", "."));
+  const additionalContribution = Number(document.getElementById("additional-contribution").value.replace(",", "."));
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(
-    document.getElementById("return-rate").value.replace(",", ".")
-  );
+  const returnRate = Number(document.getElementById("return-rate").value.replace(",", "."));
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(
-    document.getElementById("tax-rate").value.replace(",", ".")
-  );
+  const taxRate = Number(document.getElementById("tax-rate").value.replace(",", "."));
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -40,11 +36,11 @@ function renderProgression(evt) {
     additionalContribution,
     returnRate,
     returnRatePeriod
-  )
+  );
 
-  const finalInvestmentObject = returnsArray[returnsArray.length - 1]
-  
-  new Chart(finalMoneyChart, {
+  const finalInvestmentObject = returnsArray[returnsArray.length - 1];
+
+  doughnutChartReference = new Chart(finalMoneyChart, {
     type: "doughnut",
     data: {
       labels: ["Total Investido", "Rendimento", "Imposto"],
@@ -55,31 +51,27 @@ function renderProgression(evt) {
             formatCurrency(finalInvestmentObject.totalInterestReturns * (1 - taxRate / 100)),
             formatCurrency(finalInvestmentObject.totalInterestReturns * (taxRate / 100)),
           ],
-          backgroundColor: [
-            "rgb(255, 99, 132)",
-            "rgb(54, 162, 235)",
-            "rgb(255, 205, 86)"
-          ],
+          backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"],
           hoverOffset: 4,
         },
       ],
     },
   });
 
-  new Chart(progressionChart, {
-    type: 'bar',
+  progressionChartReference = new Chart(progressionChart, {
+    type: "bar",
     data: {
       labels: returnsArray.map((investmentObject) => investmentObject.month),
       datasets: [
         {
-          label: 'Total Investido',
+          label: "Total Investido",
           data: returnsArray.map((investmentObject) => formatCurrency(investmentObject.investedAmount)),
-          backgroundColor: 'rgb(255, 99, 132)',
+          backgroundColor: "rgb(255, 99, 132)",
         },
         {
-          label: 'Total Acumulado',
+          label: "Total Acumulado",
           data: returnsArray.map((investmentObject) => formatCurrency(investmentObject.interestReturns)),
-          backgroundColor: 'rgb(54, 162, 235)',
+          backgroundColor: "rgb(54, 162, 235)",
         },
       ],
     },
@@ -94,7 +86,18 @@ function renderProgression(evt) {
         },
       },
     },
-  })
+  });
+}
+
+function isObjectEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+function resetCharts() {
+  if (!isObjectEmpty(doughnutChartReference) && !isObjectEmpty(progressionChartReference)) {
+    doughnutChartReference.destroy();
+    progressionChartReference.destroy();
+  }
 }
 
 function clearForm() {
@@ -103,6 +106,7 @@ function clearForm() {
   form["time-amount"].value = "";
   form["return-rate"].value = "";
   form["tax-rate"].value = "";
+  resetCharts();
 
   const errorInputContainers = document.querySelectorAll(".error");
   for (const errorInputContainer of errorInputContainers) {
@@ -120,20 +124,13 @@ function validateInput(evt) {
   const grandParentElement = evt.target.parentElement.parentElement;
   const inputValue = evt.target.value.replace(",", ".");
 
-  if (
-    !parentElement.classList.contains("error") &&
-    (isNaN(inputValue) || Number(inputValue) <= 0)
-  ) {
+  if (!parentElement.classList.contains("error") && (isNaN(inputValue) || Number(inputValue) <= 0)) {
     const erroTextElement = document.createElement("p");
     erroTextElement.classList.add("text-red-500");
     erroTextElement.innerText = "Insira um valor númerico e maior que zero";
     parentElement.classList.add("error");
     grandParentElement.appendChild(erroTextElement);
-  } else if (
-    parentElement.classList.contains("error") &&
-    !isNaN(inputValue) &&
-    Number(inputValue) > 0
-  ) {
+  } else if (parentElement.classList.contains("error") && !isNaN(inputValue) && Number(inputValue) > 0) {
     parentElement.classList.remove("error");
     grandParentElement.querySelector("p").remove();
   }
